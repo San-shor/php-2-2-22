f9fc5a58fb87d8621da1c4e3b00c49ea84f07f05
san-shor <sanjidaewu89@gmail.com>
2022-02-03 17:22:57 +0600

    php

---
+++ app/classes/Example.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\classes;  //virtual directory
+
+class Example
+{
+    public $firstNumber=10;
+    public $firstName;
+    public $lastName;
+    public function index(){
+//        echo "hello world";
+//        echo $this->firstNumber;
+        echo 
+    }
+}
